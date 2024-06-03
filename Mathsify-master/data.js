513d0eed1f24de1d1a652d27d52b79574429a271
PrinceChineduStanley <okoriechinedustanley@gmail.com>
2024-06-03 16:33:13 +0100

    I just updated a comment

---
+++ Mathsify-master/data.js
@@ -0,0 +1,48 @@
+const data = [
+    {
+        question: Math.random() + Math.floor(Math.random() * 50) +1,
+        dp : Math.floor(Math.random() * 5) +1
+    },
+    {
+        question: Math.random() + Math.floor(Math.random() * 50) +1,
+        dp : Math.floor(Math.random() * 5) +1   
+    },
+    {
+        question: Math.random() + Math.floor(Math.random() * 50) +1,
+        dp : Math.floor(Math.random() * 5) +1   
+    },
+    {
+        question: Math.random() + Math.floor(Math.random() * 50) +1,
+        dp : Math.floor(Math.random() * 5) +1   
+    },
+    {
+        question: Math.random() + Math.floor(Math.random() * 50) +1,
+        dp : Math.floor(Math.random() * 5) +1   
+    },
+    {
+        question: Math.random() + Math.floor(Math.random() * 50) +1,
+        dp : Math.floor(Math.random() * 5) +1
+    },
+    {
+        question: Math.random() + Math.floor(Math.random() * 50) +1,
+        dp : Math.floor(Math.random() * 5) +1   
+    },
+    {
+        question: Math.random() + Math.floor(Math.random() * 50) +1,
+        dp : Math.floor(Math.random() * 5) +1   
+    },
+    {
+        question: Math.random() + Math.floor(Math.random() * 50) +1,
+        dp : Math.floor(Math.random() * 5) +1   
+    },
+    {
+        question: Math.random() + Math.floor(Math.random() * 50) +1,
+        dp : Math.floor(Math.random() * 5) +1   
+    }
+]
+
+const questions = [
+    [
+        data[0].question
+    ]
+]
